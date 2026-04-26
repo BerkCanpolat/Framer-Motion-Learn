f557892c2baec1e3e69ece0f6931adecab0adc80
Berk Canpolat <canpolatberk19@gmail.com>
2026-04-26 10:48:46 +0300

    add smooth scroll progress bar using useScroll, useSpring and useTransform

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Animationcontrols from "./Components/Animationcontrols"
 import BasicsOfMotion from "./Components/BasicsOfMotion"
 import Gestures from "./Components/Gestures"
+import ScrollAnimations from "./Components/ScrollAnimations"
 import ViewBasedAnimations from "./Components/ViewBasedAnimations"
 import Practice from "./Practice/Practice"
 
@@ -13,7 +14,8 @@ function App() {
       {/* <Practice /> */}
       {/* <Gestures /> */}
       {/* <Animationcontrols /> */}
-      <ViewBasedAnimations />
+      {/* <ViewBasedAnimations /> */}
+      <ScrollAnimations />
     </>
   )
 }
